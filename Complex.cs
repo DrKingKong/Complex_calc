0ac9989b6ee445f92f4ba2d576e0028198707af7
Mario Steiner <mariosteiner@tsn.at>
2023-12-21 19:48:03 +0100

    Lesson 21.12.

--- Complex.cs
+++ Complex.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,56 +9,212 @@ namespace Complex_calc
 {
     class Complex
     {
-        public double RE, IM, add_1, add_2, sub_1, sub2;
-        //default Konstruktor
-        public Complex()
-        {
-            //setzen von Member-VAR // Return Member-Meth
-        }
-        //überladener Konstruktor
-        public Complex(double re, double im)
-        {     
-            //if member not null...
-            RE= re;
-            IM= im;
-        }
+        // public double RE, IM, add_1, add_2, sub_1, sub2;
+        ////default Konstruktor
+        //public Complex()
+        //{
+        //    //setzen von Member-VAR // Return Member-Meth
+        //}
+        ////überladener Konstruktor
+        //public Complex(double re, double im)
+        //{     
+        //    //if member not null...
+        //    RE= re;
+        //    IM= im;
+        //}
 
-        public void Add(Complex rhs)
-        {
-            RE+= rhs.RE;
-            IM += rhs.IM;
-        }
+        //public void Add(Complex rhs)
+        //{
+        //    RE += rhs.RE;
+        //    IM += rhs.IM;
+        //}
 
-        public void Sub(Complex rhs)
-        {
-            RE -= rhs.RE;
-            IM -= rhs.IM;
-        }
+        //public void Sub(Complex rhs)
+        //{
+        //    RE -= rhs.RE;
+        //    IM -= rhs.IM;
+        //}
 
-        public void Multi(Complex rhs, Complex lhs)
-        {
-            IM = (lhs.IM * rhs.RE) + (lhs.RE * rhs.IM);
-            RE= (lhs.RE * rhs.RE) + (-1 * (lhs.IM * rhs.IM));
-        }
+        //public void Multi(Complex rhs, Complex lhs)
+        //{
+        //    IM = (lhs.IM * rhs.RE) + (lhs.RE * rhs.IM);
+        //    RE = (lhs.RE * rhs.RE) + (-1 * (lhs.IM * rhs.IM));
+        //}
 
-        public void Div(Complex lhs, Complex rhs)
-        {
-            Complex step1 = new();
-            Complex step2 = new();
-            Complex conj = new()
-            {
-                RE = rhs.RE,
-                IM = rhs.IM * -1
-            };
+        //public void Div(Complex lhs, Complex rhs)
+        //{
+        //    Complex step1 = new();
+        //    Complex step2 = new();
+        //    Complex conj = new()
+        //    {
+        //        RE = rhs.RE,
+        //        IM = rhs.IM * -1
+        //    };
 
-            step1.IM = (lhs.IM * conj.RE) + (lhs.RE * conj.IM);
-            step1.RE = (lhs.RE * conj.RE) + (-1 * (lhs.IM * conj.IM));
-            step2.RE = (rhs.RE * conj.RE) + (-1 * rhs.IM * conj.IM);
-            RE = Math.Round(step1.RE / step2.RE,2);
-            IM = Math.Round(step1.IM / step2.RE,2);
-        }
+        //    step1.IM = (lhs.IM * conj.RE) + (lhs.RE * conj.IM);
+        //    step1.RE = (lhs.RE * conj.RE) + (-1 * (lhs.IM * conj.IM));
+        //    step2.RE = (rhs.RE * conj.RE) + (-1 * rhs.IM * conj.IM);
+        //    RE = Math.Round(step1.RE / step2.RE, 2);
+        //    IM = Math.Round(step1.IM / step2.RE, 2);
+        //}
         
+        private double mRe, mIm;
+        private double mB, mW;
+        public Complex() { }
+        public Complex(double re, double im)
+            {
+                Re = re;
+                Im = im;
+            }
+        public override string ToString()
+        {
+            if (mIm >= 0)
+                return $"{mRe} + j{mIm}";
+            else
+                return $"{mRe} + j{mIm}";
+        }
+
+        public double Re
+        {
+            get
+            {
+                return mRe;
+            }
+            set
+            {
+                if (value != mRe)
+                {
+                    mRe = value;
+                    KarthToPolar();
+                }
+            }
+        }
+
+        public double Im
+        {
+            get
+            {
+                return mIm;
+            }
+            set
+            {
+                if (value != mIm)
+                {
+                    mIm = value;
+                    KarthToPolar();
+                }
+            }
+        }
+
+        public double B
+        {
+            get 
+            { 
+                return mB;
+            }
+            set
+            {
+                if(value!=mB)
+                {
+                    mB= value;
+                }
+                PolarToKarth();
+            }
+        }
+
+        public double W
+        {
+            get
+            {
+                return mW;
+            }
+            set
+            {
+                if (value != mW)
+                {
+                    mW = value;
+                }
+                PolarToKarth();
+            }
+        }
+
+        private void KarthToPolar()
+        {
+            mB=Math.Sqrt(mRe*mRe+mIm* mIm);
+            mW=Math.Atan2(mIm,mRe);
+        }
+        private void PolarToKarth()
+        {
+            mRe=mB*Math.Cos(mW);
+            mIm=mB+Math.Sin(mW);
+        }
+        public double Wdeg
+        {
+            get
+            {
+                return mW * 180.0 / Math.PI;
+            }
+            set
+            {
+                mW = value * Math.PI / 180.0;
+            }
+        }
+        public static Complex operator + (Complex lhs, Complex rhs)
+        {
+            Complex tmp = new();
+            tmp.Re = lhs.Re + rhs.Re;
+            tmp.Im=lhs.Im + rhs.Im;
+            return tmp;
+        }
+        public static Complex operator - (Complex lhs, Complex rhs)
+        {
+            Complex tmp = new();
+            tmp.Re = lhs.Re - rhs.Re;
+            tmp.Im = lhs.Im - rhs.Im;
+            return tmp;
+        }
+        public static Complex operator *(Complex lhs, Complex rhs)
+        {
+            Complex tmp = new();
+            tmp.Re = lhs.B * rhs.B;
+            tmp.Im = lhs.W + rhs.W;
+            return tmp;
+        }
+        public static Complex operator /(Complex lhs, Complex rhs)
+        {
+            Complex tmp = new();
+            tmp.Re = lhs.B / rhs.B;
+            tmp.Im = lhs.W - rhs.W;
+            return tmp;
+        }
+
+        //public Complex  Add(Complex lhs, Complex rhs)
+        //{
+        //    Complex sum = new();
+        //    sum= lhs + rhs;
+        //    return sum;
+       
+        //}
+        //public Complex  Sub(Complex lhs, Complex rhs)
+        //{
+        //    Complex diff = new();
+        //    diff=lhs - rhs;
+        //    return diff;
+        //}
+
+        //public Complex Multi(Complex lhs, Complex rhs)
+        //{
+        //    Complex mul = new();
+        //    mul = lhs * rhs;
+        //    return mul;
+        //}
+        //public Complex Div(Complex lhs, Complex rhs)
+        //{
+        //    Complex quot = new();
+        //    quot = lhs / rhs;
+        //    return quot;
+        //}
     }
-    
+
 
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,23 +26,21 @@ namespace Complex_calc
         {
             Complex add_lhs = new Complex(double.Parse(lhs_real.Text), double.Parse(lhs_imag.Text));
             Complex add_rhs = new Complex(double.Parse(rhs_real.Text), double.Parse(rhs_imag.Text));
-            add_rhs.Add(add_lhs);
-            result_LH.Text = add_rhs.RE.ToString();
-            result_RH.Text = add_rhs.IM.ToString();
+            Complex sum = new();
+            sum= add_lhs+add_rhs;
+            result_LH.Text = sum.Re.ToString();
+            result_RH.Text = sum.Im.ToString();
 
         }
 
         private void Button_Click_sub(object sender, RoutedEventArgs e)
         {
-            Complex slhs = new Complex();
-            Complex srhs = new Complex();
-            slhs.RE = double.Parse(lhs_real.Text);
-            slhs.IM = double.Parse(lhs_imag.Text);
-            srhs.RE = double.Parse(rhs_real.Text);
-            srhs.IM = double.Parse(rhs_imag.Text);
-            slhs.Sub(srhs);
-            result_LH.Text = slhs.RE.ToString();
-            result_RH.Text = slhs.IM.ToString();
+            Complex slhs = new Complex(double.Parse(lhs_real.Text), double.Parse(lhs_imag.Text));
+            Complex srhs = new Complex(double.Parse(rhs_real.Text), double.Parse(rhs_imag.Text));
+            Complex diff = new();
+            diff=slhs-srhs;
+            result_LH.Text = diff.Re.ToString();
+            result_RH.Text = diff.Im.ToString();
         }
 
         private void Button_Click_multi(object sender, RoutedEventArgs e)
@@ -50,9 +48,9 @@ namespace Complex_calc
             Complex lhs = new Complex(double.Parse(lhs_real.Text), double.Parse(lhs_imag.Text));
             Complex rhs = new Complex(double.Parse(rhs_real.Text), double.Parse(rhs_imag.Text));
             Complex multi = new();
-            multi.Multi(lhs, rhs);
-            result_LH.Text = multi.RE.ToString();
-            result_RH.Text = multi.IM.ToString();
+            multi=lhs*rhs;
+            result_LH.Text = multi.B.ToString();
+            result_RH.Text = multi.W.ToString();
 
         }
 
@@ -61,9 +59,9 @@ namespace Complex_calc
             Complex lhs = new Complex(double.Parse(lhs_real.Text), double.Parse(lhs_imag.Text));
             Complex rhs = new Complex(double.Parse(rhs_real.Text), double.Parse(rhs_imag.Text));
             Complex div = new();
-            div.Div(lhs, rhs);
-            result_LH.Text = div.RE.ToString();
-            result_RH.Text = div.IM.ToString();
+            div=lhs/rhs;
+            result_LH.Text = div.B.ToString();
+            result_RH.Text = div.W.ToString();
         }
 
         private void Button_Click_AC(object sender, RoutedEventArgs e)
@@ -76,10 +74,6 @@ namespace Complex_calc
             rhs_real.Text = "0";
         }
 
-        private void lhs_imag_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 
 
